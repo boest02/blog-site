6a18ea3685b29b8f8118b30956c5323171c05459
Stephen Boettcher <sboettcher@macbookpro.myfiosgateway.com>
2025-06-04 09:23:57 -0400

    update email and make a const

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Jessica Reilly';
+export const SITE_EMAIL = 'jessicareillypoetry@gmail.com';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
